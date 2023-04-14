270f545d815d5e5a5164c5f7cba282001788b402
John Diginee <Johndbizz@gmail.com>
2023-04-14 10:49:50 +0100

    Added views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+def post_list(request):
+    """Displays the list of posts"""
+
+    posts = Post.published.all()
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts': posts})
+
+
+def post_detail(request, id):
+    """Display single post or 404 error if post is not found."""
+
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+    
